8458f1e457cefa8a711746aabc4be432a78160ff
Payton Swick <payton@foolord.com>
2016-01-02 20:55:27 -0500

    Upgrade server to express 4

--- server/server.js
+++ server/server.js
@@ -1,43 +1,36 @@
-var http = require('http');
-var express = require('express');
-var cors = require('cors');
+var express = require( 'express' );
+var cors = require( 'cors' );
 var app = express();
-var jwt = require('express-jwt');
-var dotenv = require('dotenv');
+var jwt = require( 'express-jwt' );
+var dotenv = require( 'dotenv' );
+var bodyParser = require( 'body-parser' );
 
 dotenv.load();
 
-var authenticate = jwt({
-  secret: new Buffer(process.env.AUTH0_CLIENT_SECRET, 'base64'),
+var authenticate = jwt( {
+  secret: new Buffer( process.env.AUTH0_CLIENT_SECRET, 'base64' ),
   audience: process.env.AUTH0_CLIENT_ID
-});
+} );
 
+app.use( cors() );
+app.use( bodyParser.urlencoded() );
+app.use( bodyParser.json() );
+app.use( '/secured', authenticate );
 
-app.configure(function () {
-  app.use(cors());
+var router = express.Router();
 
- // Request body parsing middleware should be above methodOverride
-  app.use(express.bodyParser());
-  app.use(express.urlencoded());
-  app.use(express.json());
+router.get( '/ping', function( req, res ) {
+  res.status( 200 ).json( { text: "All good. You don't need to be authenticated to call this" } );
+} );
 
-  app.use('/secured', authenticate);
+router.get( '/secured/ping', function( req, res ) {
+  res.status( 200 ).json( { text: "All good. You only get this message if you're authenticated" } );
+} )
 
-
-  app.use(app.router);
-});
-
-
-app.get('/ping', function(req, res) {
-  res.send(200, {text: "All good. You don't need to be authenticated to call this"});
-});
-
-app.get('/secured/ping', function(req, res) {
-  res.send(200, {text: "All good. You only get this message if you're authenticated"});
-})
+app.use( '/', router );
 
 var port = process.env.PORT || 3001;
 
-http.createServer(app).listen(port, function (err) {
-  console.log('listening in http://localhost:' + port);
-});
+app.listen( port, function() {
+  console.log( 'listening in http://localhost:' + port );
+} );
