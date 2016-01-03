5e47e18f85bc7f1e411074d0a3b66daafec945c8
Payton Swick <payton@foolord.com>
2016-01-03 00:29:29 -0500

    Tie userId to each Location

--- server/app/models/location.js
+++ server/app/models/location.js
@@ -2,8 +2,9 @@ var mongoose = require( 'mongoose' );
 var Schema = mongoose.Schema;
 
 var LocationSchema = new Schema( {
-  name: String,
-  address: String
+  userId: { type: String, required: true },
+  name: { type: String, required: true },
+  address: { type: String, required: true },
 } );
 
 module.exports = mongoose.model( 'Location', LocationSchema );
--- server/server.js
+++ server/server.js
@@ -26,6 +26,10 @@ app.use( '/secured', authenticate );
 
 var router = express.Router();
 
+function getUserIdFromRequest( req ) {
+  return req.user.sub;
+}
+
 router.get( '/ping', function( req, res ) {
   res.status( 200 ).json( { text: "All good. You don't need to be authenticated to call this" } );
 } );
@@ -36,13 +40,19 @@ router.get( '/secured/ping', function( req, res ) {
 
 router.route( '/secured/locations' )
 .get( function( req, res ) {
-  Location.find( function( err, locations ) {
-    if ( err ) return res.status( 502 ).send( err );
+  var userId = getUserIdFromRequest( req );
+  Location.find( { userId: userId } )
+  .then( function( locations ) {
     res.status( 200 ).json( locations );
+  } )
+  .then( null, function( err ) {
+    res.status( 502 ).send( err );
   } );
 } )
 .post( function( req, res ) {
+  var userId = getUserIdFromRequest( req );
   var location = new Location();
+  location.userId = userId;
   location.name = req.body.name;
   location.address = req.body.address;
   location.save( function( err ) {
@@ -53,28 +63,38 @@ router.route( '/secured/locations' )
 
 router.route( '/secured/locations/:location_id' )
 .get( function( req, res ) {
-  Location.findById( req.params.location_id, function( err, location ) {
-    if ( err ) return res.status( 502 ).send( err );
+  var userId = getUserIdFromRequest( req );
+  Location.findOne( { _id: req.params.location_id, userId: userId } )
+  .then( function( location ) {
     res.status( 200 ).json( location );
+  } )
+  .then( null, function( err ) {
+    res.status( 502 ).send( err );
   } );
 } )
 .put( function( req, res ) {
-  Location.findById( req.params.location_id, function( err, location ) {
-    if ( err ) return res.status( 502 ).send( err );
+  var userId = getUserIdFromRequest( req );
+  Location.findOne( { _id: req.params.location_id, userId: userId } )
+  .then( function( location ) {
     location.name = req.body.name;
     location.address = req.body.address;
     location.save( function( saveErr ) {
-      if ( err ) return res.status( 502 ).send( saveErr );
+      if ( saveErr ) return res.status( 502 ).send( saveErr );
       res.status( 200 ).json( location );
     } );
   } )
+  .then( null, function( err ) {
+    res.status( 502 ).send( err );
+  } );
 } )
 .delete( function( req, res ) {
-  Location.remove( {
-    _id: req.params.location_id
-  }, function( err, location ) {
-    if ( err ) return res.status( 502 ).send( err );
+  var userId = getUserIdFromRequest( req );
+  Location.remove( { _id: req.params.location_id, userId: userId } )
+  .then( function( location ) {
     res.status( 200 ).json( location );
+  } )
+  .then( null, function( err ) {
+    res.status( 502 ).send( err );
   } );
 } );
 
