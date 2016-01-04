9b69487954164720404ba6107d8921b11587ad4e
Payton Swick <payton@foolord.com>
2016-01-03 20:15:29 -0500

    Add findOrCreate to LocationCollectionSchema

--- server/app/routes/locations.js
+++ server/app/routes/locations.js
@@ -2,38 +2,49 @@ import Location from '../models/location';
 import LocationCollection from '../models/location-collection';
 import { getUserIdFromRequest } from '../helpers';
 
+function saveNewLocation( res, location, collection ) {
+  location.save( ( err ) => {
+    if ( err ) return res.status( 502 ).send( err );
+    collection.push( location._id );
+    collection.save( ( collectionErr ) => {
+      if ( collectionErr ) return res.status( 502 ).send( collectionErr );
+      res.status( 200 ).json( location );
+    } );
+  } );
+}
+
+function removeLocation( res, locationId, userId, collection ) {
+  Location.remove( { _id: locationId, userId }, ( removeErr, location ) => {
+    if ( removeErr ) return res.status( 502 ).send( removeErr );
+    collection.save( ( saveErr ) => {
+      if ( saveErr ) return res.status( 502 ).send( saveErr );
+      res.status( 200 ).json( location );
+    } );
+  } );
+}
+
+function removeLocationFromCollection( locations, locationId ) {
+  return locations.reduce( ( collection, element ) => {
+    if ( element !== locationId ) collection.push( element );
+    return collection;
+  }, [] );
+}
+
 export default {
   list( req, res ) {
     const userId = getUserIdFromRequest( req );
-    // TODO: what if there is no LocationCollection?
-    LocationCollection.findOne( { userId } )
-    .populate( 'locations' )
-    .then( ( collection ) => {
-      res.status( 200 ).json( collection.locations );
-    } )
-    .then( null, ( err ) => {
-      res.status( 502 ).send( err );
+    LocationCollection.findOrCreate( { userId }, ( err, collection ) => {
+      res.status( 200 ).json( collection.populate( 'locations' ).locations );
     } );
   },
 
   create( req, res ) {
     const userId = getUserIdFromRequest( req );
     const { name, address } = req.body;
-    // TODO: what if there is no LocationCollection?
-    LocationCollection.findOne( { userId } )
-    .then( ( collection ) => {
+    LocationCollection.findOrCreate( { userId }, ( err, collection ) => {
+      if ( err ) return res.status( 502 ).send( err );
       const location = new Location( { userId, name, address } );
-      location.save( ( err ) => {
-        if ( err ) return res.status( 502 ).send( err );
-        collection.push( location._id );
-        collection.save( ( collectionErr ) => {
-          if ( collectionErr ) return res.status( 502 ).send( collectionErr );
-          res.status( 200 ).json( location );
-        } );
-      } );
-    } )
-    .then( null, ( err ) => {
-      res.status( 502 ).send( err );
+      saveNewLocation( res, location, collection );
     } );
   },
 
@@ -68,27 +79,10 @@ export default {
   delete( req, res ) {
     const userId = getUserIdFromRequest( req );
     const locationId = req.params.location_id;
-    // TODO: what if there is no LocationCollection?
-    LocationCollection.findOne( { userId } )
-    .populate( 'locations' )
-    .then( ( collection ) => {
-      collection.locations = collection.locations.reduce( ( coll, element ) => {
-        if ( element._id !== locationId ) coll.push( element );
-        return coll;
-      }, [] );
-      Location.remove( { _id: locationId, userId } )
-      .then( ( location ) => {
-        collection.save( ( saveErr ) => {
-          if ( saveErr ) return res.status( 502 ).send( saveErr );
-          res.status( 200 ).json( location );
-        } );
-      } )
-      .then( null, ( err ) => {
-        res.status( 502 ).send( err );
-      } );
-    } )
-    .then( null, ( err ) => {
-      res.status( 502 ).send( err );
+    LocationCollection.findOrCreate( { userId }, ( err, collection ) => {
+      if ( err ) return res.status( 502 ).send( err );
+      collection.locations = removeLocationFromCollection( collection.locations, locationId );
+      removeLocation( res, locationId, userId, collection );
     } );
   }
 };
