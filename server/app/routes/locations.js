ad141c7d7b7a252009a2c7d80699d9e772933229
Payton Swick <payton@foolord.com>
2016-01-03 19:46:02 -0500

    Delete location from LocationCollection when deleting

--- server/app/routes/locations.js
+++ server/app/routes/locations.js
@@ -5,6 +5,7 @@ import { getUserIdFromRequest } from '../helpers';
 export default {
   list( req, res ) {
     const userId = getUserIdFromRequest( req );
+    // TODO: what if there is no LocationCollection?
     LocationCollection.findOne( { userId } )
     .populate( 'locations' )
     .then( ( collection ) => {
@@ -18,15 +19,21 @@ export default {
   create( req, res ) {
     const userId = getUserIdFromRequest( req );
     const { name, address } = req.body;
-    const collection = LocationCollection.findOne( { userId } );
-    const location = new Location( { userId, name, address } );
-    location.save( ( err ) => {
-      if ( err ) return res.status( 502 ).send( err );
-      collection.push( location._id );
-      collection.save( ( collectionErr ) => {
-        if ( collectionErr ) return res.status( 502 ).send( collectionErr );
-        res.status( 200 ).json( location );
+    // TODO: what if there is no LocationCollection?
+    LocationCollection.findOne( { userId } )
+    .then( ( collection ) => {
+      const location = new Location( { userId, name, address } );
+      location.save( ( err ) => {
+        if ( err ) return res.status( 502 ).send( err );
+        collection.push( location._id );
+        collection.save( ( collectionErr ) => {
+          if ( collectionErr ) return res.status( 502 ).send( collectionErr );
+          res.status( 200 ).json( location );
+        } );
       } );
+    } )
+    .then( null, ( err ) => {
+      res.status( 502 ).send( err );
     } );
   },
 
@@ -60,10 +67,25 @@ export default {
 
   delete( req, res ) {
     const userId = getUserIdFromRequest( req );
-    // TODO: remove location_id from LocationCollection as well
-    Location.remove( { _id: req.params.location_id, userId } )
-    .then( ( location ) => {
-      res.status( 200 ).json( location );
+    const locationId = req.params.location_id;
+    // TODO: what if there is no LocationCollection?
+    LocationCollection.findOne( { userId } )
+    .populate( 'locations' )
+    .then( ( collection ) => {
+      collection.locations = collection.locations.reduce( ( coll, element ) => {
+        if ( element._id !== locationId ) coll.push( element );
+        return coll;
+      }, [] );
+      Location.remove( { _id: locationId, userId } )
+      .then( ( location ) => {
+        collection.save( ( saveErr ) => {
+          if ( saveErr ) return res.status( 502 ).send( saveErr );
+          res.status( 200 ).json( location );
+        } );
+      } )
+      .then( null, ( err ) => {
+        res.status( 502 ).send( err );
+      } );
     } )
     .then( null, ( err ) => {
       res.status( 502 ).send( err );
