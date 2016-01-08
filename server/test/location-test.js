c6c62b1334b8a582af9d8b7eb77100622fcdbc50
Payton Swick <payton@foolord.com>
2016-01-08 12:14:10 -0500

    Clean up initial tests

--- server/test/location-test.js
+++ server/test/location-test.js
@@ -12,12 +12,21 @@ const Location = require( '../app/models/location' ).default;
 const LocationCollection = require( '../app/models/location-collection' ).default;
 
 const testUserId = 'testUser';
-const homeLocation = new Location( { userId: testUserId, name: 'foo', address: 'bar' } );
-const testUserLocationCollection = new LocationCollection( { userId: testUserId, locations: [ homeLocation ] } );
+const testUserId2 = 'testUser2';
+let homeLocation;
+let workLocation;
+let testUserLocationCollection;
+let testUser2LocationCollection;
 
 function populateDb( done ) {
+  homeLocation = new Location( { userId: testUserId, name: 'foo', address: 'bar' } );
+  workLocation = new Location( { userId: testUserId2, name: 'work', address: 'place' } );
+  testUserLocationCollection = new LocationCollection( { userId: testUserId, locations: [ homeLocation ] } );
+  testUser2LocationCollection = new LocationCollection( { userId: testUserId2, locations: [ workLocation ] } );
   homeLocation.save()
+  .then( workLocation.save )
   .then( testUserLocationCollection.save )
+  .then( testUser2LocationCollection.save )
   .then( () => done() )
   .then( null, ( err ) => done( err ) );
 }
@@ -26,11 +35,12 @@ describe( 'locations', function() {
   before( function( done ) {
     mongoose.connect( 'mongodb://example.localhost/TestingDB', function( err ) {
       if ( err ) return done( err );
-      mockgoose.reset( () => populateDb( done ) );
+      done();
     } );
   } );
 
-  afterEach( function() {
+  beforeEach( function( done ) {
+    mockgoose.reset( () => populateDb( done ) );
   } );
 
   describe( '.listLocationsForUser', function() {
@@ -42,13 +52,16 @@ describe( 'locations', function() {
       locations.listLocationsForUser( testUserId )
       .then( function( data ) {
         if ( data.length === 1 && data[0].name === homeLocation.name && data[0].address === homeLocation.address ) return done();
-        done( 'location not found' );
-      } )
-      //return expect( locations.listLocationsForUser( testUserId ) ).to.eventually.have.property( 'address', homeLocation.address );
+        done( `location not found in ${JSON.stringify( data )}` );
+      } );
     } );
 
-    it( 'returns an array that does not include Locations from other users', function() {
-      return expect( locations.listLocationsForUser( 'foobar' ) ).to.eventually.not.include( homeLocation );
+    it( 'returns an array that does not include Locations from other users', function( done ) {
+      locations.listLocationsForUser( testUserId2 )
+      .then( function( data ) {
+        if ( data.length === 1 && data[0].name !== homeLocation.name && data[0].address !== homeLocation.address ) return done();
+        done( `data did not match expected data in ${JSON.stringify( data )}` );
+      } );
     } );
   } );
 } );
