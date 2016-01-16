0b8fb92f5e9577e2a334fa0151c405d4567ea552
Payton Swick <payton@foolord.com>
2016-01-16 16:41:28 -0500

    Set up login page

--- client/app/home.js
+++ client/app/home.js
@@ -8,10 +8,10 @@ export default React.createClass( {
   render: function() {
     return (
       <div className="login-box auth0-box before">
-        <img src="https://i.cloudup.com/StzWWrY34s.png" />
-        <h3>Auth0 Example</h3>
-        <p>Zero friction identity infrastructure, built for developers</p>
-        <a onClick={ this.showLock } className="btn btn-primary btn-lg btn-login btn-block">Sign In</a>
+        <img className="login-box__logo" src="/assets/logo.png" />
+        <h1 className="login-box__title">Voyageur</h1>
+        <p className="login-box__subtitle">How far do you go?</p>
+        <a onClick={ this.showLock } className="login-box__button btn btn-primary btn-lg btn-block">Let's Go!</a>
       </div>
     );
   }
