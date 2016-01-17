a8f9b57995f27937546ee4decab39e9324c0f110
Payton Swick <payton@foolord.com>
2016-01-16 19:46:48 -0500

    Add redux-localstorage

--- client/app/lib/store.js
+++ client/app/lib/store.js
@@ -1,9 +1,11 @@
 import { compose, createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import persistState from 'redux-localstorage';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = compose(
-  applyMiddleware( thunk )
+  applyMiddleware( thunk ),
+  persistState( 'auth', { key: 'voyageur' } )
 )( createStore );
 
 export default createStoreWithMiddleware( reducers );
