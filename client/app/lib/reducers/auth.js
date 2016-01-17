36e7ad8db653eb80fd2a840c9dcb1410aee40274
Payton Swick <payton@foolord.com>
2016-01-17 14:59:24 -0500

    Delete the auth token if it is expired

--- client/app/lib/reducers/auth.js
+++ client/app/lib/reducers/auth.js
@@ -1,6 +1,14 @@
+import get from 'lodash.get';
+
 const initialState = { token: null };
 export default function auth( state = initialState, action ) {
   switch ( action.type ) {
+    case 'ERROR':
+      const text = get( action, 'error.response.text', '' );
+      if ( text.match( /expired/ ) ) {
+        return Object.assign( {}, initialState );
+      }
+      break;
     case 'AUTH_GOT_TOKEN':
       return Object.assign( {}, state, { token: action.token } );
   }
