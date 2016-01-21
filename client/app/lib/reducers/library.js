47f08886e40032de02e0351e47ebbe5f2384274f
Payton Swick <payton@foolord.com>
2016-01-20 20:16:07 -0500

    Fix matchesSearch

--- client/app/lib/reducers/library.js
+++ client/app/lib/reducers/library.js
@@ -1,6 +1,6 @@
-function matchesSearch( location ) {
-  if ( this.props.searchString.length < 2 ) return true;
-  return ( ~ location.name.toLowerCase().indexOf( this.props.searchString ) || ~ location.address.toLowerCase().indexOf( this.props.searchString ) );
+function matchesSearch( searchString, location ) {
+  if ( searchString.length < 2 ) return true;
+  return ( ~ location.name.toLowerCase().indexOf( searchString ) || ~ location.address.toLowerCase().indexOf( searchString ) );
 }
 
 const initialState = { locations: [], visibleLocations: [] };
@@ -9,7 +9,7 @@ export default function library( state = initialState, action ) {
     case 'LIBRARY_GOT_LOCATIONS':
       return Object.assign( {}, { locations: action.library, visibleLocations: action.library } );
     case 'LIBRARY_SEARCH_FOR':
-      const visibleLocations = state.locations.filter( l => matchesSearch( l ) );
+      const visibleLocations = state.locations.filter( l => matchesSearch( action.searchString, l ) );
       return Object.assign( {}, state, { visibleLocations } );
     case 'LIBRARY_GOT_NEW_LOCATION':
       const locations = [ ...state.locations, action.location ];
